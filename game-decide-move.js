e418f84463e9963cb7f15797ed89a2b546b9d0e5
Daniel Venable <daniel.t.venable@gmail.com>
2020-09-26 11:56:09 -0700

    Game works. No touch screen support.

--- game-decide-move.js
+++ game-decide-move.js
@@ -1,23 +1,26 @@
 'use strict';
 
-module.exports = (allmoves, postition_score) => {
-	let data = new Map;
+const id = Symbol('gamestate ID');
+
+module.exports = allmoves => {
+	const data = new Map,
+		expand = Symbol('expand');
 
 	class TreeNode {
 		constructor(value) {
 			this.value = value;
 		}
 
-		set chlidren(value) {
-			return data.set(this.value, value);
+		set children(value) {
+			data.set(this.value[id], value);
 		}
 
-		get chlidren() {
-			return data.get(this.value) || this.#expand();
+		get children() {
+			return data.get(this.value[id]) || this[expand]();
 		}
 
 		get has_chlidren() {
-			return data.has(this.value);
+			return data.has(this.value[id]);
 		}
 
 		get_layer(layer) {
@@ -25,19 +28,23 @@ module.exports = (allmoves, postition_score) => {
 			for (var i = 0; i < layer; i++) {
 				let new_current = [];
 				for (const item of current) {
-					new_current.push(item.chlidren);
+					new_current.push(item.children);
 				}
 				current = new_current;
 			}
 			return current;
 		}
 
-		#expand() {
-			if (!this.has_chlidren) {
-				this.chlidren = allmoves(this.value).map(elem => new TreeNode(elem));
+		[expand]() {
+			const children = [];
+			for (const elem of allmoves(this.value)) {
+				children.push(new TreeNode(elem));
 			}
+			return this.children = children;
 		}
 	}
 
 	return TreeNode;
 }
+
+module.exports.gamestateID = id;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 'use strict';
 
+process.chdir(__dirname);
+
 const { createServer } = require('http'),
 	{ promises: { readFile } } = require('fs'),
+	decide_move = require('./game-decide-move.js'),
+	Move_tree = decide_move(all_moves),
+	id = decide_move.gamestateID,
 	port = process.argv[2] || 80,
-	file = readFile(__dirname + '/play.html');
+	file = readFile('./play.html');
 
 createServer(async (req, res) => {
 	if (req.method === 'GET') {
@@ -15,37 +20,96 @@ createServer(async (req, res) => {
 				res.setHeader('Content-Type', 'text/html');
 				res.end(await file);
 			} else if (state = url.match(/^[/]([xo])[/]([xo_]{9})$/)) {
-				res.setHeader('Content-Type', 'text/plain');
-				const user = state[1], grid = [...state[2]];
-				
-				res.end(grid);
+				res.setHeader('Content-Type', 'application/json');
+
+				const obj = {
+					grid: state[2],
+					token: state[1],
+					[id]: state[2] + state[1]
+				}
+				get_winner(obj);
+
+				const tree = new Move_tree(obj);
+
+				let score_list = new Set(tree.children);
+				for (const item of score_list) {
+					if (item.value.score !== undefined) score_list.delete(item);
+				}
+
+				while (!score_list[Symbol.iterator]().next().done) {
+					for (const item of score_list) {
+						let is_all_scored = true;
+						for (const child of item.children) {
+							if (child.value.score === undefined) {
+								is_all_scored = false;
+								score_list.add(child);
+							}
+						}
+						if (is_all_scored) {
+							score_by_children(item);
+							score_list.delete(item);
+						}
+					}
+				}
+
+				let best = [], best_score = -Infinity;
+				for (const child of tree.children) {
+					if (child.value.score > best_score) {
+						best = [child];
+						best_score = child.value.score;
+					} else if (child.value.score === best_score) best.push(child);
+				}
+
+				const move = best[Math.floor(Math.random() * best.length)];
+
+				res.end(JSON.stringify(move ? {
+					grid: move.value.grid,
+					winner: move.value.winner
+				} : {
+					winner: tree.value.winner
+				}));
 			} else res.statusCode = 404;
-		} catch {
+		} catch (e) {
 			res.statusCode = 400;
+			console.error(e);
 		}
 	} else res.statusCode = 405;
 	if (!res.writableEnded) res.end();
 }).listen(port, () => console.log(`Server running on http://localhost:${port}`));
 
-function* all_moves(grid, token) {
-	let i = grid.length;
-	while (i--) if (grid[i] === '_') yield replace_at(grid, token, i);
+function* all_moves({ grid, token, winner }) {
+	if (winner) return;
+	for (var i = 0; i < grid.length; i++) {
+		if (grid[i] === '_') yield get_move(grid, token, i);
+	}
 }
 
-function replace_at(str, char, position) {
-	return str.slice(0, position) + char + str.slice(position + 1);
+function get_move(grid, token, position) {
+	const obj = {
+		grid: grid.slice(0, position) + token + grid.slice(position + 1),
+		token: token === 'x' ? 'o' : 'x',
+	};
+	obj[id] = obj.grid + obj.token;
+	get_winner(obj);
+	return obj;
 }
 
-function flip(token) {
-	return token === 'x' ? 'o' : 'x';
+function get_winner(obj) {
+	const win = winner(obj.grid);
+	if (win) {
+		obj.winner = win;
+		if (win === '_') obj.score = 0;
+		else obj.score = obj.token === win ? -1 : 1;
+	}
 }
-
+winner('oxxoxxxoo');
 function winner(grid) {
-	if (grid.length !== 9) console.error('Wrong grid size!');
 	for (let is_row = 0; is_row < 2; is_row++) {
-		outer: for (let i = 0, j = 0; i < 3; i++) {
-			const get_token = is_row ? () => grid[i * 3 + j] : () => grid[j * 3 + i];
-			let current = get_token();
+		let i, j;
+		const get_token = is_row ? () => grid[i * 3 + j] : () => grid[j * 3 + i];
+		outer: for (i = 0; i < 3; i++) {
+			j = 0;
+			const current = get_token();
 			if (current === '_') continue;
 			for (j = 1; j < 3; j++) {
 				if (get_token() !== current) continue outer;
@@ -53,8 +117,13 @@ function winner(grid) {
 			return current;
 		}
 	}
-	let current = grid[4]; // center
-	if (current === '_') return null;
-	return (grid[0] === current && grid[8] === current) ||
-		(grid[2] === current && grid[6] === current) ? current : null;
+	const current = grid[4]; // center
+	if (current !== '_' && (
+		(grid[0] === current && grid[8] === current) ||
+		(grid[2] === current && grid[6] === current))) return current;
+	if (!grid.includes('_')) return '_';
+}
+
+function score_by_children(node) {
+	node.value.score = -Math.max(...node.children.map(child => child.value.score));
 }
